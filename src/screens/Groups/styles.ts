72735b87f80f48ae28edf8534c88f8616ca94f2d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-12 11:53:07 -0300

    feat: global theme

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,8 +4,10 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.COLORS.GRAY_700};
 `;
 
 export const Text = styled.Text`
   font-size: 40px;
+  color: ${(props) => props.theme.COLORS.GREEN_500};
 `;
