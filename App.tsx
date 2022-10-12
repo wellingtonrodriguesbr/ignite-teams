717979d86c88ff1c138e0427ec90e158eeed4c6a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-12 19:36:16 -0300

    style: customizing status bar

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
@@ -16,6 +17,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
