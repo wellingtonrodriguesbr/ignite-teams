bde66bbb3e44e53f2337a7c85291a9a82d41d55e
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-19 06:07:42 -0300

    chore: render list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
+import { Button } from "@components/Button";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState(["Galera da Gonew", "Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -16,7 +18,12 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhuma turma cadastrada. Que tal criar a primeira?" />
+        )}
       />
+      <Button text="Criar nova turma" />
     </Container>
   );
 }
