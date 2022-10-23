0ed39200bbbcbd72a124a2708db318ed18614985
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-23 10:52:22 -0300

    feat: create function to back home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
 import { BackButton, BackIcon, HeaderContainer, Logo } from "./styles";
+import { TouchableOpacityProps } from "react-native";
 import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 interface HeaderProps {
   showBackButton?: boolean;
 }
 
 export function Header({ showBackButton = false }: HeaderProps) {
+  const { navigate } = useNavigation();
+
+  function handleGoHome() {
+    navigate("groups");
+  }
+
   return (
     <HeaderContainer>
       {showBackButton ? (
-        <BackButton>
+        <BackButton onPress={handleGoHome}>
           <BackIcon />
         </BackButton>
       ) : null}
